814a6af2b1730b24cd5eed61a5dc35d0166a4ff7
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2024-03-14 13:51:16 -0400

    feat: update

--- start - javascript/aula07-asyncAndPromises/script.js
+++ start - javascript/aula07-asyncAndPromises/script.js
@@ -1,4 +1,4 @@
-console.log("Script iniciado");
+// console.log("Script iniciado");
 
 // setTimeout(() => {
 //   console.log("após 3 segundo");
@@ -17,3 +17,71 @@ console.log("Script iniciado");
 // console.log("Script finalizado");
 //* Promise tem 4 estados: pendente, resolvida, rejeitada ou finalizada
 
+// console.log("Iniciado");
+
+// const minhaPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("Executando a promise...");
+//     resolve("Promise resolvida");
+//   }, 3 * 1000);
+// });
+
+// minhaPromise.then((resultado) => {
+//   console.log(resultado);
+// });
+
+// console.log(minhaPromise);
+// console.log("Finalizado");
+
+//! forma de usar a Promise
+// function somaAssincrona(a, b) {
+//   return new Promise(function (resolve, reject) {
+//     const numA = parseFloat(a);
+//     const numB = parseFloat(b);
+
+//     if (isNaN(numA) || isNaN(numB)) {
+//       reject("Números inválidos!");
+//     } else {
+//       resolve(numA + numB);
+//     }
+//   });
+// }
+
+// somaAssincrona(3, 7)
+//   .then(function (resultado) {
+//     console.log(resultado);
+//   })
+//   .catch((motivo) => {
+//     console.log(motivo);
+//   })
+//   .finally(() => {
+//     console.log("Promise finalizada");
+//   });
+
+// console.log("finalizado");
+
+//* Async await
+
+function somaAssincrona(a, b) {
+  return new Promise(function (resolve, reject) {
+    const numA = parseFloat(a);
+    const numB = parseFloat(b);
+
+    if (isNaN(numA) || isNaN(numB)) {
+      reject("Números inválidos!");
+    } else {
+      resolve(numA + numB);
+    }
+  });
+}
+
+async function somaAsync(a, b) {
+  const numA = parseFloat(a);
+  const numB = parseFloat(b);
+
+  return numA + numB;
+}
+
+const resultado = somaAsync(3, 7);
+console.log(resultado);
+console.log("finalizado");
